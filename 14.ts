a0a2f258508da8c04ae63f05fa860f13d0fccc04
ad798 <72769916+ad798@users.noreply.github.com>
2024-12-16 19:57:38 -0500

    Update 14.ts

--- 14.ts
+++ 14.ts
@@ -1,8 +1,11 @@
 function minMovesToStables(reindeer: number[], stables: number[]): number {
+  // sort the arrays in ascending order
   reindeer = reindeer.sort((a, b) => a - b)
   stables = stables.sort((a, b) => a - b)
   let result = 0
   for (let i = 0; i < reindeer.length ; i++) {
+    // the movement of each reindeer is calculated by the distance
+    // of each one to the next stable
     result += Math.abs(reindeer[i] - stables[i])
   }
   return result
